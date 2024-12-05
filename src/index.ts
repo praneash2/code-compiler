5c199779d17c66300d17fccd1b0fd899dcce5c31
praneash2 <praneash2@gmail.com>
2024-12-05 23:59:52 +0530

    code compile

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
 import { runTerminalCommand } from "./utils/terminalExecution";
 
 
+const codeToBeCompiled:string =`
+print("Hello, World!")
+for i in range(5):
+    print(i)
+`;
+
+const processCode=(code:string)=>{
+
+    return code.replace(/"/g,"'");
+}
+
+
+console.log(processCode(codeToBeCompiled));
+
 (async()=>{
-    let output=await runTerminalCommand("ls")
-    console.log(output);
+    try {
+        let output=await runTerminalCommand(`docker run --rm -i python:3.9 python -c "${processCode(codeToBeCompiled)}"`)
+        console.log(output);
+        
+    } catch (error) {
+            console.error(error);
+    }   
     
+
 })()
